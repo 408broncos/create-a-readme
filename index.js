c742583e617df6c6a9bfca1ad4ccfb6e0439ab01
Jordan Cardenas <jordancardenas@Jordans-MBP.attlocal.net>
2023-04-19 15:46:05 -0700

    add questions to index.js

---
+++ index.js
@@ -0,0 +1,60 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+const Questions = [
+    {
+     name: "email",
+     message: "Enter your email" ,
+     type: "input"  
+    },
+    
+    {
+        name: "github",
+        message: "Enter you github username",
+        type: "input"
+    },
+    
+    {
+        name: "title",
+        message: "Enter the title for your project",
+        type: "input"
+    },
+
+    {
+        name: "description",
+        message: "Enter a brief description of your project",
+        type: "input"
+    },
+
+    {
+        name: "usage",
+        message: "Enter a brief summary of how you use this application",
+        type: "input"
+    },
+
+    {
+        name: "test",
+        message: "Enter how you would run tests for your project",
+        type: "input"
+    },
+
+    {
+        name: "license",
+        message: "Enter what license you are using for this project",
+        options: ["MIT", "BSD", "Eclipse", "Boost"],
+        default: "MIT",
+        type: "list"
+    },
+
+    {
+        name: "contributors",
+        message: "Enter any contributors to your project (if none put 'none')",
+        type: "input"
+    },
+
+    {
+        name: "install",
+        message: "Enter ways you installed your project",
+        type: "input"
+    }
+]
